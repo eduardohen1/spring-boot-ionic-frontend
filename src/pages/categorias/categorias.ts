dd04e772b577a9bbf716c866c0660eeaa255afc7
Eduardo Henrique Marques Ferreira <eduardohmferreira@gmail.com>
2019-01-03 16:28:54 -0200

    Criando um interceptor para tratamento de erros

--- src/pages/categorias/categorias.ts
+++ src/pages/categorias/categorias.ts
@@ -32,9 +32,7 @@ export class CategoriasPage {
       .subscribe(response => {
         this.items = response;
       },
-      error => {
-        console.log(error);
-      });
+      error => {});
   }
 
 }
