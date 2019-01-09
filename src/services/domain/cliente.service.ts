a9797d3e9960c8c9ca57fce6e6970c6d346d83b6
Eduardo Henrique Marques Ferreira <eduardohmferreira@gmail.com>
2019-01-09 16:57:00 -0200

    Buscando endereços do banco de dados

--- src/services/domain/cliente.service.ts
+++ src/services/domain/cliente.service.ts
@@ -9,8 +9,8 @@ import { StorageService } from "../storage.service";
 export class ClienteService {
    constructor(public http: HttpClient, public storage: StorageService){}
 
-   findByEmail(email: string) : Observable<ClienteDTO>{
-      return this.http.get<ClienteDTO>(`${API_CONFIG.baseUrl}/clientes/email?value=${email}`);
+   findByEmail(email: string) {
+      return this.http.get(`${API_CONFIG.baseUrl}/clientes/email?value=${email}`);
    }
 
    getImageFromBucket(id : string) : Observable<any>{
