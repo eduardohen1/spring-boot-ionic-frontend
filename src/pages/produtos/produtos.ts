a42bf79777ed441e46f3e9cb0a1a0d6d45683bfa
Eduardo Henrique Marques Ferreira <eduardohmferreira@gmail.com>
2019-01-20 01:02:09 -0200

    Componente loading

--- src/pages/produtos/produtos.ts
+++ src/pages/produtos/produtos.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, LoadingController } from 'ionic-angular';
 import { ProdutoDTO } from '../../models/produto.dto';
 import { API_CONFIG } from '../../config/api.config';
 import { ProdutoService } from '../../services/domain/produto.service';
@@ -16,16 +16,21 @@ export class ProdutosPage {
   constructor(
     public navCtrl: NavController, 
     public navParams: NavParams,
-    public produtoService: ProdutoService) {
+    public produtoService: ProdutoService,
+    public loadingCtrl: LoadingController) {
   }
 
   ionViewDidLoad() {
     let categoria_id = this.navParams.get('categoria_id');
+    let loader = this.presentLoading();
     this.produtoService.findByCategoria(categoria_id)
         .subscribe(response => {
           this.items = response['content'];
+          loader.dismiss();
           this.loadImageUrls();
-        }, error =>{});
+        }, error =>{
+          loader.dismiss();
+        });
   }
 
   loadImageUrls(){
@@ -41,6 +46,14 @@ export class ProdutosPage {
 
   showDetail(produto_id : string){
     this.navCtrl.push('ProdutoDetailPage', {produto_id : produto_id});
+  }
+
+  presentLoading() {
+    const loader = this.loadingCtrl.create({
+      content: "Aguarde..."      
+    });
+    loader.present();
+    return loader;
   }
 
 }
