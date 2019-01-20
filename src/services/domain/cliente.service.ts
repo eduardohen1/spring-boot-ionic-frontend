e9c930164a9d40d596a7abb5e6d716a5398245b6
Eduardo Henrique Marques Ferreira <eduardohmferreira@gmail.com>
2019-01-20 00:05:20 -0200

    Tela de confirmação de pedido

--- src/services/domain/cliente.service.ts
+++ src/services/domain/cliente.service.ts
@@ -9,6 +9,10 @@ import { StorageService } from "../storage.service";
 export class ClienteService {
    constructor(public http: HttpClient, public storage: StorageService){}
 
+   findById(id: string) {
+      return this.http.get(`${API_CONFIG.baseUrl}/clientes/${id}`);
+   }
+
    findByEmail(email: string) {
       return this.http.get(`${API_CONFIG.baseUrl}/clientes/email?value=${email}`);
    }
