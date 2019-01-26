43dcfc9b351f76f957146202b5ef9d0212454104
Eduardo Henrique Marques Ferreira <eduardohmferreira@gmail.com>
2019-01-26 01:16:45 -0200

    Ajustes para publicação

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,4 +1,4 @@
 export const API_CONFIG = {
-   baseUrl: "http://localhost:8080",
+   baseUrl: "https://cursospringionic201810.herokuapp.com",
    bucketBaseUrl: "https://s3-sa-east-1.amazonaws.com/ehsolucoes-spring-ionic"
 }
