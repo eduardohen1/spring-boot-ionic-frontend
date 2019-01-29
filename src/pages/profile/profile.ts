1a58d67093755df9ed9edc5c1ebbc6b891850c7f
Eduardo Henrique Marques Ferreira <eduardohmferreira@gmail.com>
2019-01-29 01:41:28 -0200

    Cancelamento da camera ou galeria

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -67,6 +67,7 @@ export class ProfilePage {
      this.picture = 'data:image/png;base64,' + imageData;
      this.cameraOn = false;
     }, (err) => {     
+      this.cameraOn = false;
     });
   }
 
@@ -84,6 +85,7 @@ export class ProfilePage {
      this.picture = 'data:image/png;base64,' + imageData;
      this.cameraOn = false;
     }, (err) => {     
+      this.cameraOn = false;
     });
   }
 
